310a84ba122a066e4d089ac6eda7608a7b49df79
Dasha Tarasova <tdariaa@mail.ru>
2023-07-12 05:26:36 +0300

    style adaptiv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,22 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
 
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <header className="header">
+    <header className={`header ${open ? 'header_open' : ''}`}>
       <img className="header__logo" src={logo} alt="Логотип." />
       <div className="header__info">
-        {props.userData && <p className="header__info header__info_type_email">{props.userData.data.email}</p>}
-
         {props.link ? <Link to={props.link} className="header__info header__info_type_auth">{props.linkTitle}</Link> :
-          <button className="header__info header__info_type_signout" onClick={props.onExit}>{props.linkTitle}</button>}
+          <>
+          <button className={`header__burger-button ${open ? 'header__burger-button_open' : ''}`} onClick={()=>{setOpen(!open)}}></button>
+            <div className={`header__info-container ${open ? 'header__info-container_open' : ''}`}>
+              {props.userData && <p className="header__info header__info_type_email">{props.userData.data.email}</p>}
+              <button className="header__info header__info_type_signout" onClick={() => {props.onExit(); setOpen(false)}}>{props.linkTitle}</button>
+            </div>
+          </>
+        }
+
       </div>
     </header>
   );
